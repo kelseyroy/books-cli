f4577e103bfd84c5b446c398280730441425bee9
kroy94 <kelseytylerroy@gmail.com>
2022-12-18 14:54:18 -0800

    cleaning up some comments and console.logs

--- spec/keywordSearchQuery.spec.js
+++ spec/keywordSearchQuery.spec.js
@@ -1,11 +1,12 @@
-
+const MockAPI = 
 describe('Keyword Search', function(){
-    let search;
-    beforeEach(function(){
-        search = new Search();
-        search.title = "Pride and Prejudice";
-        search.authors = "Jane Austen";
-        search.categories = "Fiction";
-        search.isbn_13 = "9780486284736";
-    })
+    // let search;
+    // beforeEach(function(){
+    //     search = new Search();
+    //     search.title = "Pride and Prejudice";
+    //     search.authors = "Jane Austen";
+    //     search.categories = "Fiction";
+    //     search.isbn_13 = "9780486284736";
+    // })
+    it('should return 200 response code',)
 })
