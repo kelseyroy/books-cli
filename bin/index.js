21622e7be0480c70b8c9a64097f8d1ec8212e16e
kroy94 <kelseytylerroy@gmail.com>
2022-12-18 17:09:30 -0800

    troubleshooting jasmine tests

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
-const search = require('../lib/commands/search');
+const {search, viewReadingList} = require('../lib/commands')
 
 
-const viewReadingList = () => console.log('READING LIST')
+// const viewReadingList = () => console.log('READING LIST')
 
 const argv = yargs
     .usage('Usage: $0 <cmd> [options]')
-    .command('search', 'use inquirer to prompt for your name', () => { }, search)
+    .command('search', 'search will prompt the user to input a keyword, title, author, subject, or isbn number. Users can hit enter to skip.', () => { }, search)
     .command('view', 'a classic yargs command without prompting', () => { }, viewReadingList)
     .demandCommand(1, 1, 'choose a command: search or view')
     .help('h').argv;
