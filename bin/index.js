261f56099fc4bae49e0afe782f5093b548af0245
kroy94 <kelseytylerroy@gmail.com>
2022-12-19 17:15:20 -0500

    Have completed the search and list functionality. Moving on to working on save command

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
-const {search, viewReadingList} = require('../lib/commands')
-
+const {search, list} = require('../lib/commands')
 
 // const viewReadingList = () => console.log('READING LIST')
 
 const argv = yargs
     .usage('Usage: $0 <cmd> [options]')
     .command('search', 'search will prompt the user to input a keyword, title, author, subject, or isbn number. Users can hit enter to skip.', () => { }, search)
-    .command('view', 'a classic yargs command without prompting', () => { }, viewReadingList)
+    .command('save', 'save books from your last search', () => { }, viewReadingList)
+    .command('list', 'view your saved books', () => { }, list)
     .demandCommand(1, 1, 'choose a command: search or view')
     .help('h').argv;
