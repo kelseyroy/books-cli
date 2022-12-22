4867e6f1943f2d4518080f4bc194f0347301465d
kroy94 <kelseytylerroy@gmail.com>
2022-12-22 15:05:52 -0500

    finalized save functionality and added comments to code for readability

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
-const {search, list} = require('../lib/commands')
-
-// const viewReadingList = () => console.log('READING LIST')
+const {search, list, save} = require('../lib/commands')
 
 const argv = yargs
     .usage('Usage: $0 <cmd> [options]')
-    .command('search', 'search will prompt the user to input a keyword, title, author, subject, or isbn number. Users can hit enter to skip.', () => { }, search)
-    .command('save', 'save books from your last search', () => { }, viewReadingList)
+    .command('search', 'search will prompt the user to input a keyword, title, author, subject, or isbn number. Users can hit enter to skip a search field.', () => { }, search)
+    .command('save', 'save books from your last search', () => { }, save)
     .command('list', 'view your saved books', () => { }, list)
-    .demandCommand(1, 1, 'choose a command: search or view')
+    .demandCommand(1, 1, 'choose a command: search, save or list')
     .help('h').argv;
