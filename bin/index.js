b0730c224ca771190ea917b6ca6bfac58e6d4f4a
kroy94 <kelseytylerroy@gmail.com>
2023-01-05 02:29:26 -0500

    updated all cjs modules to es modules formatting.

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const booksCli = require('../lib/commands');
+// const booksCli = require('../lib/commands');
+import {booksCli} from "../lib/commands/index.js"
 
 booksCli();
 
